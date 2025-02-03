428aa7a837233747b9fa5d987f7fa15bf878d41b
Leon Blakey <code@leonblakey.com>
2025-02-02 19:15:47 -0800

    strum dependency

--- commons/src/err_utils.rs
+++ commons/src/err_utils.rs
@@ -2,7 +2,7 @@ use std::backtrace::Backtrace;
 use std::error::{Error, request_ref};
 use std::io;
 use std::marker::PhantomData;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 /// Make a message and a back trace look pretty
 pub fn pretty_error(e: impl Error) -> String {
@@ -69,3 +69,38 @@ impl<E> IOEC<E> {
         }
     }
 }
+
+// TODO: could this ever work?
+// pub trait IOECMap<S, E> {
+//     fn map(&self) -> impl FnOnce(S) -> E;
+// }
+//
+// impl<E> IOECMap<io::Error, E> for IOEC<E>
+// where
+//     E: From<IOECStd>,
+// {
+//     fn map(&self) -> impl FnOnce(io::Error) -> E {
+//         |err: io::Error| {
+//             IOECStd {
+//                 path: self.path.clone(),
+//                 err,
+//             }
+//             .into()
+//         }
+//     }
+// }
+//
+// impl<E> IOECMap<serde_json::Error, E> for IOEC<E>
+// where
+//     E: From<IOECSerde>,
+// {
+//     fn map(&self) -> impl FnOnce(serde_json::Error) -> E {
+//         |err: serde_json::Error| {
+//             IOECSerde {
+//                 path: self.path.clone(),
+//                 err,
+//             }
+//             .into()
+//         }
+//     }
+// }
