8fb6f1f73c0c9018e10b679f71a83ca6c9e396cf
Leon Blakey <code@leonblakey.com>
2025-11-29 01:38:36 -0800

    stor: schema rewrite

--- stor_diesel/src/schema.rs
+++ stor_diesel/src/schema.rs
@@ -1,103 +1,156 @@
-// @generated automatically by Diesel CLI.
+//
+// Injest
+//
 
 diesel::table! {
-    alabel_names (xrn_label_name) {
+    publish_log (publish_id) {
+        publish_id -> Unsigned<Integer>,
+        at -> Timestamp,
         #[max_length = 100]
-        xrn_label_name -> Varchar,
+        cause_xrn -> Nullable<Varchar>,
+        cause_description -> Text,
+    }
+}
+
+diesel::table! {
+    injest_types (injest_type) {
+        injest_type -> Unsigned<Integer>,
+        #[max_length = 20]
+        name -> Varchar,
+    }
+}
+
+//
+
+diesel::table! {
+    injest_data_immutable (injest_id) {
+        publish_id -> Unsigned<Integer>,
+        injest_id -> Unsigned<Integer>,
+        injest_type -> Unsigned<Integer>,
+        data -> Blob,
+    }
+}
+diesel::joinable!(injest_data_immutable -> publish_log (publish_id));
+// diesel::allow_tables_to_appear_in_same_query!(injest_data_immutable, publish_log);
+
+diesel::table! {
+    injest_data_upgraded (injest_id) {
+        publish_id -> Unsigned<Integer>,
+        injest_id -> Unsigned<Integer>,
+        injest_type -> Unsigned<Integer>,
+        data -> Blob,
+    }
+}
+diesel::joinable!(injest_data_upgraded -> publish_log (publish_id));
+// diesel::allow_tables_to_appear_in_same_query!(injest_data_upgraded, publish_log);
+
+diesel::table! {
+    injest_complete (injest_id) {
+        publish_id -> Unsigned<Integer>,
+        injest_id -> Unsigned<Integer>,
+    }
+}
+diesel::joinable!(injest_complete -> publish_log (publish_id));
+diesel::joinable!(injest_complete -> injest_data_immutable (injest_id));
+diesel::joinable!(injest_complete -> injest_data_upgraded (injest_id));
+// diesel::allow_tables_to_appear_in_same_query!(
+//     injest_complete,
+//     publish_log,
+//     injest_data_immutable,
+//     injest_data_upgraded
+// );
+
+//
+// Space
+//
+
+diesel::table! {
+    space (space_id) {
+        publish_id -> Unsigned<Integer>,
+        space_id -> Unsigned<Integer>,
+        #[max_length = 50]
+        project_name -> Varchar,
         description -> Text,
-        #[max_length = 25]
-        published -> Varchar,
-        publish_cause -> Text,
     }
 }
+diesel::joinable!(space -> publish_log (publish_id));
 
 diesel::table! {
-    aproject_lasers (xrn_laser_id) {
-        xrn_laser_id -> Unsigned<Integer>,
-        title -> Text,
+    space_owned (space_id) {
+        publish_id -> Unsigned<Integer>,
+        space_id -> Unsigned<Integer>,
         description -> Text,
-        #[max_length = 25]
-        published -> Varchar,
-        publish_cause -> Text,
     }
 }
+diesel::joinable!(space_owned -> publish_log (publish_id));
+
+//
+// hd1
+//
 
 diesel::table! {
-    aproject_names (xrn_project_id) {
-        xrn_project_id -> Unsigned<Integer>,
-        title -> Text,
+    hd1_sites (hd_site_id) {
+        hd_site_id -> Unsigned<Integer>,
+        #[max_length = 50]
+        site_name -> Varchar,
         description -> Text,
-        #[max_length = 25]
-        published -> Varchar,
-        publish_cause -> Text,
     }
 }
 
 diesel::table! {
-    fire_history (xrn_firehist_id) {
-        xrn_firehist_id -> Unsigned<Integer>,
-        #[max_length = 13]
-        fire_id -> Varchar,
-        fire_last_visit -> Unsigned<Integer>,
-        visit_count -> Unsigned<Integer>,
-        title -> Text,
-        #[max_length = 25]
-        published -> Varchar,
-        publish_cause -> Text,
+    hd1_galleries (hd_id) {
+        publish_id -> Unsigned<Integer>,
+        project_id -> Unsigned<Integer>,
+        hd_site_id -> Unsigned<Integer>,
+        hd_id -> Unsigned<Integer>,
+        #[max_length = 50]
+        tor_hash -> Binary,
+    }
+}
+diesel::joinable!(hd1_galleries -> publish_log (publish_id));
+diesel::joinable!(hd1_galleries -> hd1_sites (hd_site_id));
+
+//
+// tor
+//
+
+diesel::table! {
+    tor1_status_types (publish_id) {
+        tor_status_type -> Unsigned<Integer>,
+        #[max_length = 50]
+        name -> Varchar
     }
 }
 
 diesel::table! {
-    jnl_id_counters (key) {
-        #[max_length = 40]
-        key -> Varchar,
-        counter -> Unsigned<Integer>,
-        #[max_length = 25]
-        updated -> Varchar,
+    tor1_qb_host (publish_id) {
+        qb_host_id -> Unsigned<Integer>,
+        #[max_length = 50]
+        name -> Varchar
     }
 }
 
+//
+
 diesel::table! {
-    jnl_mutation (mut_id) {
-        mut_id -> Unsigned<Integer>,
-        #[max_length = 40]
-        mut_type -> Varchar,
-        data -> Text,
-        #[max_length = 25]
-        published -> Varchar,
-        publish_cause -> Text,
+    tor1_torrents (torhash) {
+        publish_id -> Unsigned<Integer>,
+        space_id -> Unsigned<Integer>,
+        #[max_length = 50]
+        torhash -> Binary,
+        tor_status_type -> Unsigned<Integer>,
+        tor_status_changed -> Timestamp,
+        qb_host_id -> Unsigned<Integer>,
     }
 }
 
-diesel::table! {
-    registry_ids (xrn) {
-        #[max_length = 100]
-        xrn -> Varchar,
-        #[max_length = 25]
-        published -> Varchar,
-        publish_cause -> Text,
-    }
-}
-
-diesel::table! {
-    registry_links (xrn_source) {
-        #[max_length = 100]
-        xrn_source -> Varchar,
-        #[max_length = 100]
-        xrn_target -> Varchar,
-        #[max_length = 25]
-        published -> Varchar,
-        publish_cause -> Text,
-    }
-}
+//
+// Other
+//
 
 diesel::allow_tables_to_appear_in_same_query!(
-    alabel_names,
-    aproject_lasers,
-    aproject_names,
-    fire_history,
-    jnl_id_counters,
-    jnl_mutation,
-    registry_ids,
-    registry_links,
+    injest_complete,
+    publish_log,
+    injest_data_immutable,
+    injest_data_upgraded
 );
