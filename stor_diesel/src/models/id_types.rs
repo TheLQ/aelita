b451e982d3b2a5bebd2b16248d4035b12dd83192
Leon Blakey <code@leonblakey.com>
2025-11-29 14:01:42 -0800

    stor: id types work in conversions

--- stor_diesel/src/models/id_types.rs
+++ stor_diesel/src/models/id_types.rs
@@ -3,12 +3,19 @@ use byteorder::WriteBytesExt;
 use diesel::deserialize::FromSql;
 use diesel::mysql::{Mysql, MysqlValue};
 use diesel::serialize::{IsNull, Output, ToSql};
-use diesel::sql_types::{Integer, Unsigned};
+use diesel::sql_types::{Integer, Text, Unsigned};
+use diesel::{AsExpression, FromSqlRow};
+use std::io::Write;
+use strum::{AsRefStr, EnumString};
 
 macro_rules! id_type {
     ($name:ident) => {
-        #[derive(Debug)]
+        // #[derive(Debug, AsExpression, diesel::FromSqlRow)]
+        // #[diesel(sql_type = Integer)]
+        #[derive(Debug, diesel::FromSqlRow)]
         pub struct $name(u32);
+
+        // core conversions
 
         impl FromSql<Unsigned<Integer>, Mysql> for $name {
             fn from_sql(bytes: MysqlValue) -> diesel::deserialize::Result<Self> {
@@ -23,10 +30,76 @@ macro_rules! id_type {
                 Ok(IsNull::No)
             }
         }
+
+        // Manual #[derive(AsExpression)] as it picks Integer not Unsigned<Integer>
+
+        impl diesel::expression::AsExpression<Unsigned<Integer>> for $name {
+            type Expression =
+                diesel::internal::derives::as_expression::Bound<Unsigned<Integer>, Self>;
+            fn as_expression(self) -> Self::Expression {
+                Self::Expression::new(self)
+            }
+        }
+
+        impl diesel::expression::AsExpression<Unsigned<Integer>> for &$name {
+            type Expression =
+                diesel::internal::derives::as_expression::Bound<Unsigned<Integer>, Self>;
+            fn as_expression(self) -> Self::Expression {
+                Self::Expression::new(self)
+            }
+        }
+
+        // seems unnessesary
+
+        // impl From<u32> for $name {
+        //     fn from(v: u32) -> Self {
+        //         Self(v)
+        //     }
+        // }
+        //
+
+        // impl From<$name> for u32 {
+        //     fn from(value: $name) -> u32 {
+        //         value.0
+        //     }
+        // }
     };
 }
 id_type!(ModelPublishId);
 id_type!(ModelJournalId);
+id_type!(ModelJournalType);
+
+// #[derive(Debug, AsExpression, diesel::FromSqlRow)]
+// // #[derive(Debug, diesel::FromSqlRow)]
+// #[diesel(sql_type = Integer)]
+// pub struct ModelPublishId(u32);
+//
+// impl diesel::expression::AsExpression<Unsigned<Integer>> for ModelPublishId {
+//     type Expression = diesel::internal::derives::as_expression::Bound<Unsigned<Integer>, Self>;
+//     fn as_expression(self) -> Self::Expression {
+//         Self::Expression::new(self)
+//     }
+// }
+//
+// impl From<ModelPublishId> for u32 {
+//     fn from(value: ModelPublishId) -> u32 {
+//         value.0
+//     }
+// }
+//
+// impl FromSql<Unsigned<Integer>, Mysql> for ModelPublishId {
+//     fn from_sql(bytes: MysqlValue) -> diesel::deserialize::Result<Self> {
+//         let t = <u32 as FromSql<Unsigned<Integer>, Mysql>>::from_sql(bytes)?;
+//         Ok(Self(t))
+//     }
+// }
+//
+// impl ToSql<Unsigned<Integer>, Mysql> for ModelPublishId {
+//     fn to_sql<'b>(&'b self, out: &mut Output<'b, '_, Mysql>) -> diesel::serialize::Result {
+//         out.write_u32::<NativeEndian>(self.0)?;
+//         Ok(IsNull::No)
+//     }
+// }
 
 // #[derive(Debug)]
 // pub struct TestId(u32);
