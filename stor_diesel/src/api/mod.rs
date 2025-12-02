22d0eb1160202c3b7622b8ce6bc71fcf6510366e
Leon Blakey <code@leonblakey.com>
2025-12-01 21:52:30 -0800

    stor: remove old queries

--- stor_diesel/src/api/mod.rs
+++ stor_diesel/src/api/mod.rs
@@ -1,7 +1,4 @@
 pub mod api_journal;
-pub mod api_project;
-pub mod api_project_laser;
-pub mod api_registry_ids;
 pub mod api_space;
 mod common;
 
--- stor_diesel/src/models/mod.rs
+++ stor_diesel/src/models/mod.rs
@@ -4,10 +4,7 @@ mod macro_gen;
 pub mod date;
 pub mod id_types;
 pub mod model_journal;
-pub mod model_project_laser;
-pub mod model_project_names;
-pub mod model_registry_ids;
 pub mod model_space;
 
-pub use model_project_names::*;
-pub use model_registry_ids::*;
+// pub use model_project_names::*;
+// pub use model_registry_ids::*;
