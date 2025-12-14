74ef5b354d5f58b9dca6c3fb14f5108d608101f9
Leon Blakey <code@leonblakey.com>
2025-12-13 23:52:30 -0800

    stor: split into 4_init_hd

---
+++ stor_diesel/migrations/4_init_hd/down.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS `hd1_galleries`;
+DROP TABLE IF EXISTS `hd1_sites`;
+DROP TABLE IF EXISTS `hd1_files_tree`;
+DROP TABLE IF EXISTS `hd1_files_linear`;
+DROP TABLE IF EXISTS `hd1_files_parents`;
---
+++ stor_diesel/migrations/4_init_hd/up.sql
@@ -0,0 +1,36 @@
+CREATE TABLE `hd1_sites`
+(
+    `journal_id`  INTEGER UNSIGNED NOT NULL,
+    `hd_site_id`  INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `site_name`   VARCHAR(50)      NOT NULL,
+    `description` TEXT             NOT NULL,
+    FOREIGN KEY (`journal_id`) REFERENCES `journal_immutable` (`journal_id`)
+);
+
+CREATE TABLE `hd1_galleries`
+(
+    `journal_id` INTEGER UNSIGNED NOT NULL,
+    `hd_site_id` INTEGER UNSIGNED NOT NULL,
+    `hd_id`      INTEGER UNSIGNED NOT NULL PRIMARY KEY,
+    `tor_hash`   BINARY(50)       NOT NULL,
+    FOREIGN KEY (`journal_id`) REFERENCES `journal_immutable` (`journal_id`),
+    FOREIGN KEY (`hd_site_id`) REFERENCES `hd1_sites` (`hd_site_id`)
+);
+
+CREATE TABLE `hd1_files_linear`
+(
+    `path` VARCHAR(600) NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE `hd1_files_tree`
+(
+    `id`        INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `component` VARBINARY(250)   NOT NULL UNIQUE
+);
+
+CREATE TABLE `hd1_files_parents`
+(
+    `id`        INTEGER UNSIGNED NOT NULL PRIMARY KEY,
+    `parent_id` INTEGER UNSIGNED,
+    INDEX (`parent_id`)
+);
