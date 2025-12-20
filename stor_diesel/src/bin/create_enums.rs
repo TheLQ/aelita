10b63c3ca79b5711cc5d512d4df1d029f78150b8
Leon Blakey <code@leonblakey.com>
2025-12-20 02:01:39 -0800

    stor: update valid enums v3

--- stor_diesel/src/bin/create_enums.rs
+++ stor_diesel/src/bin/create_enums.rs
@@ -3,16 +3,14 @@ extern crate core;
 use aelita_commons::log_init;
 use aelita_stor_diesel::{
     ModelJournalTypeName, PermaStore, StorDieselError, StorDieselResult, StorTransaction,
-    establish_connection, show_create_table,
+    establish_connection,
 };
 use aelita_xrn::defs::address::XrnType;
 use aelita_xrn::defs::path_xrn::PathXrnType;
 use aelita_xrn::defs::space_xrn::SpaceXrnType;
 use diesel::RunQueryDsl;
-use postcard::to_vec;
 use std::process::ExitCode;
 use strum::VariantArray;
-use xana_commons_rs::tracing_re::info;
 use xana_commons_rs::{CommaJoiner, pretty_main};
 
 fn main() -> ExitCode {
@@ -24,25 +22,15 @@ fn main() -> ExitCode {
 pub fn run() -> StorDieselResult<()> {
     let conn = &mut establish_connection(PermaStore::AelitaNull).unwrap();
 
-    // StorTransaction::new_transaction("build", conn, |conn| {
-    //     let create = show_create_table(conn.inner(), "space_owned")?;
-    //     info!("create {create}");
-    //     Ok::<(), StorDieselError>(())
-    // })?;
     StorTransaction::new_transaction("build", conn, |conn| {
-        // let create = show_create_table(conn.inner(), "space_owned")?;
-        // info!("create {create}");
-
-        let primary_keys = type_names(<XrnType as strum::VariantNames>::VARIANTS);
+        let primary_keys = type_names::<XrnType>();
         diesel::sql_query(alter_enum_query("space_owned", "child_type1", primary_keys))
             .execute(conn.inner())?;
 
-        let secondary_keys = type_names(
-            SpaceXrnType::VARIANTS
-                .iter()
-                .map(AsRef::as_ref)
-                .chain(PathXrnType::VARIANTS.iter().map(AsRef::as_ref)),
-        );
+        let mut secondary_keys = String::new();
+        secondary_keys.push_str(&type_names::<SpaceXrnType>());
+        secondary_keys.push_str(", ");
+        secondary_keys.push_str(&type_names::<PathXrnType>());
         diesel::sql_query(alter_enum_query(
             "space_owned",
             "child_type2",
@@ -50,7 +38,7 @@ pub fn run() -> StorDieselResult<()> {
         ))
         .execute(conn.inner())?;
 
-        let journal_keys = type_names(ModelJournalTypeName::VARIANTS.iter().map(|v| v.as_ref()));
+        let journal_keys = type_names::<ModelJournalTypeName>();
         diesel::sql_query(alter_enum_query(
             "journal_immutable",
             "journal_type",
@@ -64,8 +52,9 @@ pub fn run() -> StorDieselResult<()> {
     Ok(())
 }
 
-fn type_names<'a>(from: impl IntoIterator<Item = impl AsRef<str>>) -> String {
-    from.into_iter()
+fn type_names<'a, V: VariantArray + AsRef<str>>() -> String {
+    V::VARIANTS
+        .into_iter()
         .map(|v| format!("'{}'", v.as_ref()))
         .collect::<CommaJoiner>()
         .value()
