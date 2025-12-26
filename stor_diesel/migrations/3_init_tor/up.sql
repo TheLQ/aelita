4e49987fa44d72b35ebca5a2230564aea5efba2c
Leon Blakey <code@leonblakey.com>
2025-12-26 03:02:45 -0800

    stor: improve sql enum formatting

--- stor_diesel/migrations/3_init_tor/up.sql
+++ stor_diesel/migrations/3_init_tor/up.sql
@@ -8,27 +8,28 @@ CREATE TABLE `tor1_qb_host`
 DROP TABLE `tor1_torrents`;
 CREATE TABLE `tor1_torrents`
 (
-    -- @formatter:off for massive enum
-    `journal_id`         INTEGER UNSIGNED    NOT NULL,
-    `qb_host_id`         INTEGER UNSIGNED    NOT NULL,
-    `infohash_v1`        BINARY(20)          NOT NULL,
-    `infohash_v2`        BINARY(32)          NOT NULL,
-    `name`               TEXT                NOT NULL,
-    `comment`            TEXT                NOT NULL,
-    `path`               TEXT                NOT NULL,
-    `progress`           FLOAT               NOT NULL,
-    `original_size`      BIGINT UNSIGNED,
-    `selected_size`      BIGINT UNSIGNED,
-    `downloaded`         BIGINT UNSIGNED     NOT NULL,
-    `uploaded`           BIGINT UNSIGNED     NOT NULL,
-    `secs_active`        INTEGER UNSIGNED    NOT NULL,
-    `secs_seeding`       INTEGER UNSIGNED    NOT NULL,
-    `added_on`           TIMESTAMP           NOT NULL,
-    `completion_on`      TIMESTAMP,
-    `state`         ENUM('error', 'missingFiles', 'uploading', 'pausedUP', 'queuedUP', 'stalledUP', 'checkingUP', 'forcedUP', 'allocating', 'metaDL', 'downloading', 'pausedDL', 'queuedDL', 'stalledDL', 'checkingDL', 'forcedDL', 'checkingResumeData', 'moving', 'unknown', 'stoppedDL', 'stoppedUP') NOT NULL,
-    -- @formatter:on
+    `journal_id`    INTEGER UNSIGNED NOT NULL,
+    `qb_host_id`    INTEGER UNSIGNED NOT NULL,
+    `infohash_v1`   BINARY(20)       NOT NULL,
+    `infohash_v2`   BINARY(32)       NOT NULL,
+    `name`          TEXT             NOT NULL,
+    `comment`       TEXT             NOT NULL,
+    `path`          TEXT             NOT NULL,
+    `progress`      FLOAT            NOT NULL,
+    `original_size` BIGINT UNSIGNED,
+    `selected_size` BIGINT UNSIGNED,
+    `downloaded`    BIGINT UNSIGNED  NOT NULL,
+    `uploaded`      BIGINT UNSIGNED  NOT NULL,
+    `secs_active`   INTEGER UNSIGNED NOT NULL,
+    `secs_seeding`  INTEGER UNSIGNED NOT NULL,
+    `added_on`      TIMESTAMP        NOT NULL,
+    `completion_on` TIMESTAMP,
+    `state`         ENUM ('dummy')   NOT NULL,
     PRIMARY KEY (`infohash_v1`),
     CONSTRAINT `fk_tor1_torrents_journal`
         FOREIGN KEY (`journal_id`) REFERENCES `journal_immutable` (`journal_id`)
 );
-show create table `tor1_torrents`
+# show create table `tor1_torrents`
+ALTER TABLE `tor1_torrents`
+    MODIFY `state`
+        ENUM ('error', 'missingFiles', 'uploading', 'pausedUP', 'queuedUP', 'stalledUP', 'checkingUP', 'forcedUP', 'allocating', 'metaDL', 'downloading', 'pausedDL', 'queuedDL', 'stalledDL', 'checkingDL', 'forcedDL', 'checkingResumeData', 'moving', 'unknown', 'stoppedDL', 'stoppedUP') NOT NULL;
