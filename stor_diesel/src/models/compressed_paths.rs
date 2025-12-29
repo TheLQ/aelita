327041982af152be4333d01b3e0b1d5b29fa82b4
Leon Blakey <code@leonblakey.com>
2025-12-29 00:06:18 -0800

    stor: improve compressed paths with cache

--- stor_diesel/src/models/compressed_paths.rs
+++ stor_diesel/src/models/compressed_paths.rs
@@ -1,22 +1,24 @@
 use crate::StorDieselResult;
 use crate::err::StorDieselErrorKind;
 use indexmap::IndexSet;
+use rayon::prelude::ParallelSliceMut;
 use serde::{Deserialize, Serialize};
-use std::ffi::{OsStr, OsString};
-use std::os::unix::ffi::OsStrExt;
+use std::ffi::OsString;
 use std::path::{Component, Path, PathBuf};
-use xana_commons_rs::num_format_re::ToFormattedString;
-use xana_commons_rs::tracing_re::{info, trace, warn};
- use xana_commons_rs::{CrashErrKind, LOCALE, ProgressWidget, ScanFileType, ScanFileTypeWithPath};
+use xana_commons_rs::tracing_re::{info, warn};
+use xana_commons_rs::{CrashErrKind, ProgressWidget, ScanFileType, ScanFileTypeWithPath};
 
 #[derive(Serialize)]
 pub struct CompressedPathNested {
     parts: Vec<OsString>,
-    nodes: CompNode,
+    nodes: Vec<CompNode>,
 }
 
 impl CompressedPathNested {
-    pub fn from_scan(scans: Vec<ScanFileTypeWithPath>) -> Self {
+    pub fn from_scan(mut scans: Vec<ScanFileTypeWithPath>) -> Self {
+        info!("starting sort");
+        scans.par_sort();
+
         let mut build = CompressedPathNestedBuilder::new();
         let total_scans = scans.len();
         let mut progress = ProgressWidget::new(4096);
@@ -30,141 +32,127 @@ impl CompressedPathNested {
     }
 
     fn from_build(
-        CompressedPathNestedBuilder { parts, nodes }: CompressedPathNestedBuilder,
+        CompressedPathNestedBuilder {
+            parts,
+            nodes,
+            cache: _,
+        }: CompressedPathNestedBuilder,
     ) -> Self {
         Self {
             parts: parts.into_iter().collect(),
-            nodes: CompNode::from_builder(nodes, &mut PathBuf::from("/")),
+            nodes: nodes
+                .into_iter()
+                .map(|v| CompNode::from_builder(v, &mut PathBuf::from("/")))
+                .collect(),
         }
     }
 }
-
 struct CompressedPathNestedBuilder {
     parts: IndexSet<OsString>,
-    nodes: CompNodeBuilder,
+    nodes: Vec<CompNodeBuilder>,
+    /// By pre-sorting the input paths we can cache eg 9/10 components
+    /// Vastly improving performance with 30 million paths
+    /// 10k/sec to 250k/sec
+    cache: Vec<CachedLookup>,
 }
 
 impl CompressedPathNestedBuilder {
     fn new() -> Self {
         Self {
             parts: IndexSet::new(),
-            nodes: CompNodeBuilder {
-                name: usize::MAX,
+            nodes: vec![CompNodeBuilder {
                 node_type: None,
-                children_ids: Vec::new(),
-                children: Vec::new(),
-            },
+                children_comp_ids: Vec::new(),
+                children_indexes: Vec::new(),
+            }],
+            cache: Vec::new(),
         }
     }
 
     fn push_path(&mut self, path: &Path, new_node_type: ScanFileType) {
         let mut comps = path.components();
         assert_eq!(comps.next(), Some(Component::RootDir));
+        self.add_node_linear(0, comps, new_node_type, path)
+    }
 
-        self.nodes
-            // .add_node_recursive(comps, new_node_type, &mut self.parts, path);
-            .add_node_linear(comps, new_node_type, &mut self.parts, path);
+    fn add_node_linear<'c>(
+        &mut self,
+        start_index: usize,
+        comps: impl Iterator<Item = Component<'c>>,
+        new_node_type: ScanFileType,
+        debug_source: &Path,
+    ) {
+        let mut last_index = start_index;
+        for (i, comp) in comps.enumerate() {
+            let Component::Normal(comp) = comp else {
+                panic!("Valid path {}", debug_source.display())
+            };
+
+            let mut is_clear_cache = false;
+            if let Some(cache) = self.cache.get(i) {
+                if cache.component == comp {
+                    // yay
+                    last_index = cache.child_id;
+                    continue;
+                } else {
+                    is_clear_cache = true;
+                }
+            }
+            if is_clear_cache {
+                self.cache.truncate(i);
+            }
+
+            let comp_index = self.parts.get_index_of(comp).unwrap_or_else(|| {
+                self.parts.insert(comp.to_os_string());
+                self.parts.len() - 1
+            });
+
+            if let Some(pos) = self.nodes[last_index]
+                .children_comp_ids
+                .iter()
+                .rposition(|v| *v == comp_index)
+            {
+                last_index = self.nodes[last_index].children_indexes[pos];
+            } else {
+                let next_index = self.nodes.len();
+                self.nodes[last_index].children_comp_ids.push(comp_index);
+                self.nodes[last_index].children_indexes.push(next_index);
+                self.nodes.push(CompNodeBuilder {
+                    node_type: None,
+                    children_comp_ids: Vec::new(),
+                    children_indexes: Vec::new(),
+                });
+                last_index = next_index;
+            };
+
+            self.cache.push(CachedLookup {
+                child_id: last_index,
+                component: comp.to_os_string(),
+            })
+        }
+        let last = &mut self.nodes[last_index];
+        last.node_type = Some(new_node_type);
     }
 }
 
 struct CompNodeBuilder {
-    name: usize,
     node_type: Option<ScanFileType>,
-    children_ids: Vec<usize>,
-    children: Vec<CompNodeBuilder>,
-}
-
-impl CompNodeBuilder {
-    pub fn add_node_linear<'c>(
-        &mut self,
-        mut comps: impl Iterator<Item = Component<'c>>,
-        new_node_type: ScanFileType,
-        parts: &mut IndexSet<OsString>,
-        debug_source: &Path,
-    ) {
-        let mut last = self;
-        for comp in comps {
-            let Component::Normal(comp) = comp else {
-                panic!("Valid path {}", debug_source.display())
-            };
-            let comp_index = parts.get_index_of(comp).unwrap_or_else(|| {
-                parts.insert(comp.to_os_string());
-                parts.len() - 1
-            });
-
-            let pos = if let Some(pos) = last.children_ids.iter().rposition(|v| *v == comp_index) {
-                pos
-            } else {
-                last.children_ids.push(comp_index);
-                last.children.push(Self {
-                    name: comp_index,
-                    node_type: None,
-                    children_ids: Vec::new(),
-                    children: Vec::new(),
-                });
-                last.children.len() - 1
-            };
-
-            last = &mut last.children[pos];
-        }
-        last.node_type = Some(new_node_type);
-    }
-
-    pub fn add_node_recursive<'c>(
-        &mut self,
-
-        mut comps: impl Iterator<Item = Component<'c>>,
-        new_node_type: ScanFileType,
-        parts: &mut IndexSet<OsString>,
-        debug_source: &Path,
-    ) -> Option<ScanFileType> {
-        let Some(next) = comps.next() else {
-            return Some(new_node_type);
-        };
-        let Component::Normal(comp) = next else {
-            panic!("Valid path {}", debug_source.display())
-        };
-        let comp_index = parts.get_index_of(comp).unwrap_or_else(|| {
-            parts.insert(comp.to_os_string());
-            parts.len() - 1
-        });
-
-        let next_level = if let Some(pos) = self.children_ids.iter().rposition(|v| *v == comp_index)
-        {
-            &mut self.children[pos]
-        } else {
-            self.children_ids.push(comp_index);
-            self.children.push(Self {
-                name: comp_index,
-                node_type: None,
-                children_ids: Vec::new(),
-                children: Vec::new(),
-            });
-            self.children.last_mut().unwrap()
-        };
-
-        let is_last = next_level.add_node_recursive(comps, new_node_type, parts, debug_source);
-        if let Some(new_node_type) = is_last {
-            next_level.node_type = Some(new_node_type);
-        }
-        None
-    }
+    children_indexes: Vec<usize>,
+    children_comp_ids: Vec<usize>,
 }
 
 #[derive(Serialize, Deserialize)]
 struct CompNode {
-    name: usize,
     node_type: ScanFileType,
-    children: Vec<CompNode>,
+    children_indexes: Vec<usize>,
 }
 
 impl CompNode {
     fn from_builder(
         CompNodeBuilder {
-            name,
             node_type,
-            children,
-            children_ids: _,
+            children_indexes,
+            children_comp_ids,
         }: CompNodeBuilder,
         debug_context: &mut PathBuf,
     ) -> Self {
@@ -176,20 +164,19 @@ impl CompNode {
             // }
             ScanFileType::Dir
         });
-
-        let children = children
-            .into_iter()
-            .map(|v| CompNode::from_builder(v, debug_context))
-            .collect();
         // if debug_context != Path::new("/") {
         //     assert!(debug_context.pop(), "cur {}", debug_context.display());
         // }
         Self {
-            name,
             node_type,
-            children,
+            children_indexes,
         }
     }
+}
+
+struct CachedLookup {
+    component: OsString,
+    child_id: usize,
 }
 
 #[derive(Debug)]
