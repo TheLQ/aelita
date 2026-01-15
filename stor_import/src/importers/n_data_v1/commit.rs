d9368caba7cbfd969f495381a47d84969867780e
Leon Blakey <code@leonblakey.com>
2026-01-15 03:51:42 -0800

    stor: (integ) fix commit zstd decompress now

--- stor_import/src/importers/n_data_v1/commit.rs
+++ stor_import/src/importers/n_data_v1/commit.rs
@@ -1,9 +1,10 @@
 use crate::err::{StorImportErrorKind, StorImportResult};
-use aelita_stor_diesel::ModelJournalImmutable;
 use aelita_stor_diesel::ModelJournalTypeName;
 use aelita_stor_diesel::StorTransaction;
 use aelita_stor_diesel::storapi_hd_tree_push;
-use xana_commons_rs::ResultXanaMap;
+use aelita_stor_diesel::{ModelJournalImmutable, RawDieselBytes};
+use std::collections::VecDeque;
+use xana_commons_rs::{CrashErrKind, ResultXanaMap};
 use xana_fs_indexer_rs::CompressedPaths;
 
 pub fn storcommit_hd(
@@ -12,10 +13,14 @@ pub fn storcommit_hd(
 ) -> StorImportResult<()> {
     assert_eq!(row.journal_type, ModelJournalTypeName::NData1);
 
-    let compressed: CompressedPaths = row
-        .data
+    let raw_compressed = zstd::decode_all(VecDeque::from(row.data.0)).map_err(|e| {
+        StorImportErrorKind::InvalidCompressedPaths.build_message(format!("zstd failed with {e}"))
+    })?;
+
+    let compressed: CompressedPaths = RawDieselBytes(raw_compressed)
         .deserialize_postcard()
         .xana_err(StorImportErrorKind::InvalidCompressedPaths)?;
+
     storapi_hd_tree_push(conn, compressed)?;
 
     Ok(())
